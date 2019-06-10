f6ee18d01f8e01929d91d80843ad375e0e51a1ab
Sarah Sicard <sarah.sicard2@gmail.com>
2019-06-10 09:37:20 -0500

    work on shapefiles

--- js/app.js
+++ js/app.js
@@ -80,85 +80,85 @@ legend.onAdd = function (map) {
 }
 
 info.addTo(map);
-
-// custom zoom bar control that includes a Zoom Home function
-L.Control.zoomHome = L.Control.extend({
-    options: {
-        position: 'topleft',
-        zoomInText: '<b>+</b>',
-        zoomInTitle: 'Zoom in',
-        zoomOutText: '<b>-</b>',
-        zoomOutTitle: 'Zoom out',
-        zoomHomeText: '<i class="fa fa-home" style="line-height:1.65;"></i>',
-        zoomHomeTitle: 'Zoom home'
-    },
-
-    onAdd: function (map) {
-        var controlName = 'gin-control-zoom',
-            container = L.DomUtil.create('div', controlName + ' leaflet-bar'),
-            options = this.options;
-
-        this._zoomInButton = this._createButton(options.zoomInText, options.zoomInTitle,
-        controlName + '-in', container, this._zoomIn);
-        this._zoomHomeButton = this._createButton(options.zoomHomeText, options.zoomHomeTitle,
-        controlName + '-home', container, this._zoomHome);
-        this._zoomOutButton = this._createButton(options.zoomOutText, options.zoomOutTitle,
-        controlName + '-out', container, this._zoomOut);
-
-        this._updateDisabled();
-        map.on('zoomend zoomlevelschange', this._updateDisabled, this);
-
-        return container;
-    },
-
-    onRemove: function (map) {
-        map.off('zoomend zoomlevelschange', this._updateDisabled, this);
-    },
-
-    _zoomIn: function (e) {
-        this._map.zoomIn(e.shiftKey ? 3 : 1);
-    },
-
-    _zoomOut: function (e) {
-        this._map.zoomOut(e.shiftKey ? 3 : 1);
-    },
-
-    _zoomHome: function (e) {
-        map.setView([lat, lng], zoom);
-    },
-
-    _createButton: function (html, title, className, container, fn) {
-        var link = L.DomUtil.create('a', className, container);
-        link.innerHTML = html;
-        link.href = '#';
-        link.title = title;
-
-        L.DomEvent.on(link, 'mousedown dblclick', L.DomEvent.stopPropagation)
-            .on(link, 'click', L.DomEvent.stop)
-            .on(link, 'click', fn, this)
-            .on(link, 'click', this._refocusOnMap, this);
-
-        return link;
-    },
-
-    _updateDisabled: function () {
-        var map = this._map,
-            className = 'leaflet-disabled';
-
-        L.DomUtil.removeClass(this._zoomInButton, className);
-        L.DomUtil.removeClass(this._zoomOutButton, className);
-
-        if (map._zoom === map.getMinZoom()) {
-            L.DomUtil.addClass(this._zoomOutButton, className);
-        }
-        if (map._zoom === map.getMaxZoom()) {
-            L.DomUtil.addClass(this._zoomInButton, className);
-        }
-    }
-});
-// add the new control to the map
-var zoomHome = new L.Control.zoomHome();
-zoomHome.addTo(map);
+//
+// // custom zoom bar control that includes a Zoom Home function
+// L.Control.zoomHome = L.Control.extend({
+//     options: {
+//         position: 'topleft',
+//         zoomInText: '<b>+</b>',
+//         zoomInTitle: 'Zoom in',
+//         zoomOutText: '<b>-</b>',
+//         zoomOutTitle: 'Zoom out',
+//         zoomHomeText: '<i class="fa fa-home" style="line-height:1.65;"></i>',
+//         zoomHomeTitle: 'Zoom home'
+//     },
+//
+//     onAdd: function (map) {
+//         var controlName = 'gin-control-zoom',
+//             container = L.DomUtil.create('div', controlName + ' leaflet-bar'),
+//             options = this.options;
+//
+//         this._zoomInButton = this._createButton(options.zoomInText, options.zoomInTitle,
+//         controlName + '-in', container, this._zoomIn);
+//         this._zoomHomeButton = this._createButton(options.zoomHomeText, options.zoomHomeTitle,
+//         controlName + '-home', container, this._zoomHome);
+//         this._zoomOutButton = this._createButton(options.zoomOutText, options.zoomOutTitle,
+//         controlName + '-out', container, this._zoomOut);
+//
+//         this._updateDisabled();
+//         map.on('zoomend zoomlevelschange', this._updateDisabled, this);
+//
+//         return container;
+//     },
+//
+//     onRemove: function (map) {
+//         map.off('zoomend zoomlevelschange', this._updateDisabled, this);
+//     },
+//
+//     _zoomIn: function (e) {
+//         this._map.zoomIn(e.shiftKey ? 3 : 1);
+//     },
+//
+//     _zoomOut: function (e) {
+//         this._map.zoomOut(e.shiftKey ? 3 : 1);
+//     },
+//
+//     _zoomHome: function (e) {
+//         map.setView([lat, lng], zoom);
+//     },
+//
+//     _createButton: function (html, title, className, container, fn) {
+//         var link = L.DomUtil.create('a', className, container);
+//         link.innerHTML = html;
+//         link.href = '#';
+//         link.title = title;
+//
+//         L.DomEvent.on(link, 'mousedown dblclick', L.DomEvent.stopPropagation)
+//             .on(link, 'click', L.DomEvent.stop)
+//             .on(link, 'click', fn, this)
+//             .on(link, 'click', this._refocusOnMap, this);
+//
+//         return link;
+//     },
+//
+//     _updateDisabled: function () {
+//         var map = this._map,
+//             className = 'leaflet-disabled';
+//
+//         L.DomUtil.removeClass(this._zoomInButton, className);
+//         L.DomUtil.removeClass(this._zoomOutButton, className);
+//
+//         if (map._zoom === map.getMinZoom()) {
+//             L.DomUtil.addClass(this._zoomOutButton, className);
+//         }
+//         if (map._zoom === map.getMaxZoom()) {
+//             L.DomUtil.addClass(this._zoomInButton, className);
+//         }
+//     }
+// });
+// // add the new control to the map
+// var zoomHome = new L.Control.zoomHome();
+// zoomHome.addTo(map);
 
 
 
@@ -313,3 +313,20 @@ $(document).ready( function() {
     });
 
 });
+
+//=============================== Shape Files ==========================================//
+var shpfile = new L.Shapefile('gas_and_oil_fields.zip', {
+			onEachFeature: function(feature, layer) {
+				if (feature.properties) {
+					layer.bindPopup(Object.keys(feature.properties).map(function(k) {
+						return k + ": " + feature.properties[k];
+					}).join("<br />"), {
+						maxHeight: 200
+					});
+				}
+			}
+		});
+		shpfile.addTo(map);
+    shpfile.once("data:loaded", function() {
+      console.log("finished loaded shapefile");
+  });
